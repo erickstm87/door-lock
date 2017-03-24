6430615db596b52f91962ded3253f08dd8476563
Thomas Erickson <terickson@spotxchange.com>
2017-03-23 19:52:11 -0600

    think i have this working with socket client

---
+++ client.js
@@ -0,0 +1,21 @@
+// Connect to server
+var io = require('socket.io-client');
+const express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 4390));
+var socket = io.connect(`https://guarded-ravine-69960.herokuapp.com:${port}`, {reconnect: true});
+
+console.log('2');
+
+// Add a connect listener
+socket.on('connect', function(socket) {
+  console.log('Connected!');
+});
+
+console.log('3');
+
+
+
+
+//https://guarded-ravine-69960.herokuapp.com/
