3bb90647ce8473d1430bc1ad34b04f510e7b0f3c
Thomas Erickson <terickson@spotxchange.com>
2017-04-01 14:29:04 -0600

    andrew mead help

--- client.js
+++ client.js
@@ -1,12 +1,16 @@
 // Connect to server
 console.log('1');
-var socket = require('socket.io-client')('http://guarded-ravine-69960.herokuapp.com/command', {reconnect: true});
+var socket = require('socket.io-client')('https://guarded-ravine-69960.herokuapp.com/', {reconnect: true});
 //var socket = io();
 
 console.log('2');
 
 // Add a connect listener
 socket.on('anEvent', function(msg){
+  console.log('here is your message', msg);
+});
+
+socket.on('aMessage', function(msg){
   console.log('here is your message', msg);
 });
 
