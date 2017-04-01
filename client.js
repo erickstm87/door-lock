a968f69451c00901a6c6cdf97f70db7455c0076a
Thomas Erickson <terickson@spotxchange.com>
2017-04-01 14:09:31 -0600

    socket working not

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 // Connect to server
 console.log('1');
-var socket = io('https://guarded-ravine-69960.herokuapp.com/', {reconnect: true});
+var socket = io('https://guarded-ravine-69960.herokuapp.com/socket.io/socket.io.js', {reconnect: true});
 //var socket = io();
 
 console.log('2');
