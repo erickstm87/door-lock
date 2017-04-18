a6552bcb76cda8445e8f11c12d17edf07f2bd8c7
Thomas Erickson <terickson@spotxchange.com>
2017-04-18 08:59:27 -0600

    added a dummy config file for those wondering what it would look like

---
+++ dummyConfig.js
@@ -0,0 +1,5 @@
+//This is to see what my local config looks like
+module.exports = {
+  "secretPin" : "a salted pin xxxxxxxxxxx",
+  "heroUrl":"my heroku url. This could be salted to keep others off of your socket but I can't see why it would be totally necessary"
+}
