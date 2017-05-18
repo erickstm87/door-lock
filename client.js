56042d8bff507e4e714b0c2941b69fb07132a197
Thomas <erickstm87@github.com>
2017-05-18 01:09:28 +0000

    added in some more setup

--- client.js
+++ client.js
@@ -1,10 +1,27 @@
 var io = require('socket.io-client'),
     localConfigs = require('./config'),
     socket = io(localConfigs.heroUrl),
-    bcrypt = require('bcryptjs'),
-    onoff = require('onoff');
-    Gpio = require('pigpio').Gpio,
-    led = new Gpio(17, {mode: Gpio.OUTPUT}),
+    bcrypt = require('bcryptjs');
+
+//setup my servo
+var unlockedState = 1000;
+var lockedState = 2200;
+
+var motorPin = 14;
+var buttonPin = 4
+var ledPin = 17
+
+var locked = true
+
+//setting up gpio down below    
+var Gpio = require('pigpio').Gpio,
+    motor = new Gpio(motorPin, {mode: Gpio.OUTPUT}),
+    button = new Gpio(buttonPin, {
+      mode: Gpio.INPUT,
+      pullUpDown: Gpio.PUD_DOWN,
+      edge: Gpio.FALLING_EDGE
+    }),
+    led = new Gpio(ledPin, {mode: Gpio.OUTPUT});
     dutyCycle = 0;
 
 var light = function(){
@@ -17,6 +34,20 @@ var light = function(){
     }
   }, 20);
 };
+
+//button setup here
+button.on('interrupt', function () {
+	//console.log("level: " + level + " locked: " + locked)
+	//if (level == 0) {
+	//	if (locked) {
+	//		unlockDoor()
+	//	} else {
+	///		lockDoor()
+	//	}
+	//}
+	console.log('pushed a button');
+	light();
+});
 
 socket.on('connect', function(){
   console.log('connected to heroku app');
